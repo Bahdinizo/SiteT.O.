e5667af477af56c334c2ff98610cceb9bfe525fd
bahdinizo <t.barbara.dutradinizo@gmail.com>
2023-05-14 22:33:02 -0300

    adição de carrosel

---
+++ index.js
@@ -0,0 +1,21 @@
+function funcao2()
+{alert("Você será redirecionado para a página de integração sensorial");}
+function funcao1()
+{alert("Você será redirecionado para a página de início");}
+
+var carrosel = document.querySelector('.manual')
+var cont = 1
+document.getElementById('carrosel1').checked = true
+
+function proximaImg(){
+    cont++
+    if( cont > 3){
+        cont = 1;
+    }
+    document.getElementById('carrosel'+cont).checked = true  
+      
+}
+
+setInterval(() => {
+    proximaImg()
+}, 3000)
