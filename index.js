2b7be6c81f1bc6493bf12d397f1396437ffedaac
bahdinizo <t.barbara.dutradinizo@gmail.com>
2023-06-20 15:29:48 -0300

    responsividade

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
-function funcao2()
-{alert("Você será redirecionado para a página de integração sensorial");}
-function funcao1()
-{alert("Você será redirecionado para a página de início");}
+function funcao2() {
+  alert("Você será redirecionado para a página de integração sensorial");
+}
+function funcao1() {
+  alert("Você será redirecionado para a página de início");
+}
 
-var carrosel = document.querySelector('.manual')
-var cont = 1
-document.getElementById('carrosel1').checked = true
+var carrosel = document.querySelector(".manual");
+var cont = 1;
+document.getElementById("carrosel1").checked = true;
 
-function proximaImg(){
-    cont++
-    if( cont > 3){
-        cont = 1;
-    }
-    document.getElementById('carrosel'+cont).checked = true  
-      
+function proximaImg() {
+  cont++;
+  if (cont > 3) {
+    cont = 1;
+  }
+  document.getElementById("carrosel" + cont).checked = true;
 }
 
 setInterval(() => {
-    proximaImg()
-}, 3000)
+  proximaImg();
+}, 3000);
